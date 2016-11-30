791380d3cd6e0f8166d7c2308b44c4aedca77173
My Kim Bui <mykimbuibui@gmail.com>
2016-11-30 13:57:16 +0100

    bookings

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,7 +1,7 @@
 class Language < ApplicationRecord
   LANGUAGES = %w(English French Chinese Dutch Italian Spanish Arabic)
-  has_many :users, through: :user_languages, inclusion: { in: LANGUAGES }
+  has_many :users, through: :user_languages
   has_many :user_languages
 
-  validates :name, presence: true
+  validates :name, presence: true, inclusion: { in: LANGUAGES }
 end
